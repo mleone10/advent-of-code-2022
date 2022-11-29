04a80be247e96a3a56597e029f741e530f1b975c
mleone10 <mleone10@gmail.com>
2022-11-28 23:30:58 -0500

    Save progress on newday script

--- cmd/newday/newday.go
+++ cmd/newday/newday.go
@@ -12,6 +12,9 @@ import (
 //go:embed day.go.template
 var dayTemplate string
 
+// TODO: Refactor this to parse flags then hand off to helper client or something testable
+// TODO: Implement tests for helper client
+// TODO: Order should be 1) create directory, 2) create files, 3) write files
 func main() {
 	day := flag.Int("day", 0, "day number to initialize")
 	flag.Parse()
@@ -20,7 +23,7 @@ func main() {
 		log.Fatalf("invalid day number provided (must be between 1 and 25)")
 	}
 
-	err := os.Mkdir(fmt.Sprintf("src/day%02d", *day), 755)
+	err := os.Mkdir(fmt.Sprintf("src/day%02d", *day), 0755)
 	if err != nil {
 		log.Fatalf("failed to create new day directory: %v", err)
 	}
