b9f38b4e3eac56ec21d8cb6016324687d018db7f
Mario Leone <mleone10@gmail.com>
2022-12-05 09:14:00 -0500

    Day 4: Refactor to be smarter, move to embedded inputs

--- src/day04/day04.go
+++ src/day04/day04.go
@@ -5,18 +5,24 @@ import (
 	"strings"
 )
 
-func ConvertToRange(s string) []int {
-	start, end := getStartEnd(s)
-	var span []int
-	for i := start; i <= end; i++ {
-		span = append(span, i)
-	}
-	return span
+type Day04 struct {
+	R1 rng
+	R2 rng
 }
 
-func getStartEnd(s string) (int, int) {
-	parts := strings.Split(s, "-")
-	start, _ := strconv.Atoi(parts[0])
-	end, _ := strconv.Atoi(parts[1])
-	return start, end
+type rng struct {
+	Start, End int
+}
+
+func New(line string) Day04 {
+	return Day04{
+		newRng(strings.Split(line, ",")[0]),
+		newRng(strings.Split(line, ",")[1]),
+	}
+}
+
+func newRng(span string) rng {
+	start, _ := strconv.Atoi(strings.Split(span, "-")[0])
+	end, _ := strconv.Atoi(strings.Split(span, "-")[1])
+	return rng{start, end}
 }
