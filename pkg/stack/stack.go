b8b695d9de3bcb36e66e944cefbae8329f780e11
mleone10 <mleone10@gmail.com>
2022-12-05 12:57:13 -0500

    Extend stack with New, Fill, Peek, Copy methods

--- pkg/stack/stack.go
+++ pkg/stack/stack.go
@@ -4,6 +4,12 @@ type Stack[T any] struct {
 	stack []T
 }
 
+func New[T comparable](items ...T) Stack[T] {
+	var s Stack[T]
+	s.Fill(items...)
+	return s
+}
+
 func (s *Stack[T]) Push(item T) {
 	s.stack = append(s.stack, item)
 }
@@ -16,4 +22,21 @@ func (s *Stack[T]) Pop() T {
 
 func (s *Stack[T]) Len() int {
 	return len(s.stack)
+}
+
+func (s *Stack[T]) Fill(items ...T) {
+	for _, i := range items {
+		s.Push(i)
+	}
+}
+
+func (s *Stack[T]) Peek() T {
+	return s.stack[len(s.stack)-1]
+}
+
+func (s *Stack[T]) Copy() Stack[T] {
+	var cpy Stack[T]
+	cpy.stack = make([]T, len(s.stack))
+	copy(cpy.stack, s.stack)
+	return cpy
 }
--- pkg/stack/stack_test.go
+++ pkg/stack/stack_test.go
@@ -7,6 +7,13 @@ import (
 	"github.com/mleone10/advent-of-code-2022/pkg/stack"
 )
 
+func TestNew(t *testing.T) {
+	s := stack.New(1, 2, 3, 4, 5)
+
+	assert.Equal(t, s.Len(), 5)
+	assert.Equal(t, s.Pop(), 5)
+}
+
 func TestPush(t *testing.T) {
 	var s stack.Stack[int]
 
@@ -35,4 +42,35 @@ func TestLen(t *testing.T) {
 	}
 
 	assert.Equal(t, s.Len(), 10)
+}
+
+func TestFill(t *testing.T) {
+	var s stack.Stack[int]
+
+	s.Fill(1, 2, 3, 4, 5)
+	assert.Equal(t, s.Len(), 5)
+	assert.Equal(t, s.Pop(), 5)
+}
+
+func TestPeek(t *testing.T) {
+	var s stack.Stack[int]
+
+	s.Push(1)
+	s.Push(2)
+
+	assert.Equal(t, s.Peek(), 2)
+	assert.Equal(t, s.Len(), 2)
+}
+
+func TestCopy(t *testing.T) {
+	var s stack.Stack[int]
+
+	s.Push(1)
+	s.Push(2)
+
+	r := s.Copy()
+
+	assert.NotEqual(t, &s, &r)
+	assert.Equal(t, s.Len(), r.Len())
+	assert.Equal(t, s.Peek(), r.Peek())
 }
