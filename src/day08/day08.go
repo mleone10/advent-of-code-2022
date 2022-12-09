b2674b184057375c4db0e1c0e6360e333961baa2
mleone10 <mleone10@gmail.com>
2022-12-08 19:38:23 -0500

    Persist work on day 8

--- src/day08/day08.go
+++ src/day08/day08.go
@@ -1,5 +1,51 @@
 package day08
 
+import (
+	"strconv"
+	"strings"
+
+	"github.com/mleone10/advent-of-code-2022/pkg/array"
+	"github.com/mleone10/advent-of-code-2022/pkg/grid"
+)
+
 type Day08 struct {
-  Input string
+	Input string
+}
+
+func NewGrid(input string) grid.Plane[int] {
+	g := grid.Plane[int]{}
+	for i, row := range strings.Split(strings.TrimSpace(input), "\n") {
+		for j, col := range strings.Split(row, "") {
+			height, _ := strconv.Atoi(col)
+			g.Set(j, i, height)
+		}
+	}
+	return g
+}
+
+func IsVisible(g grid.Plane[int], x, y int) bool {
+	if x == 0 || y == 0 || x == g.Width()-1 || y == g.Height()-1 {
+		// Point is the edge of the map, thus is visible
+		return true
+	}
+	return isVisibleFromRight(g, x, y) ||
+		isVisibleFromLeft(g, x, y) ||
+		isVisibleFromTop(g, x, y) ||
+		isVisibleFromBottom(g, x, y)
+}
+
+func isVisibleFromTop(g grid.Plane[int], x, y int) bool {
+	return array.Max(g.Col(x)[:y]) < g.Get(x, y)
+}
+
+func isVisibleFromBottom(g grid.Plane[int], x, y int) bool {
+	return array.Max(g.Col(x)[y+1:]) < g.Get(x, y)
+}
+
+func isVisibleFromLeft(g grid.Plane[int], x, y int) bool {
+	return array.Max(g.Row(y)[:x]) < g.Get(x, y)
+}
+
+func isVisibleFromRight(g grid.Plane[int], x, y int) bool {
+	return array.Max(g.Row(y)[x+1:]) < g.Get(x, y)
 }
--- src/day08/day08_test.go
+++ src/day08/day08_test.go
@@ -3,6 +3,9 @@ package day08_test
 import (
 	_ "embed"
 	"testing"
+
+	"github.com/mleone10/advent-of-code-2022/pkg/assert"
+	"github.com/mleone10/advent-of-code-2022/src/day08"
 )
 
 //go:embed test_input.txt
@@ -15,8 +18,58 @@ var tcs = []struct {
 	input           string
 	expectedPartOne int
 	expectedPartTwo int
-}{}
+}{
+	{
+		input:           testInput,
+		expectedPartOne: 21,
+	},
+	{
+		input:           input,
+		expectedPartOne: 1698,
+	},
+}
 
-func TestSolvePartOne(t *testing.T) {}
+func TestNewGrid(t *testing.T) {
+	g := day08.NewGrid(testInput)
+	assert.Equal(t, g.Get(0, 0), 3)
+	assert.Equal(t, g.Get(1, 1), 5)
+	assert.Equal(t, g.Get(2, 2), 3)
+	assert.Equal(t, g.Get(4, 3), 9)
+}
+
+func TestHidden(t *testing.T) {
+	g := day08.NewGrid(testInput)
+	// Outer edge is always visible
+	assert.Equal(t, day08.IsVisible(g, 0, 0), true)
+	assert.Equal(t, day08.IsVisible(g, 3, 4), true)
+	assert.Equal(t, day08.IsVisible(g, 4, 4), true)
+
+	assert.Equal(t, day08.IsVisible(g, 1, 1), true)
+	assert.Equal(t, day08.IsVisible(g, 2, 1), true)
+	assert.Equal(t, day08.IsVisible(g, 3, 1), false)
+
+	assert.Equal(t, day08.IsVisible(g, 1, 2), true)
+	assert.Equal(t, day08.IsVisible(g, 2, 2), false)
+	assert.Equal(t, day08.IsVisible(g, 3, 2), true)
+
+	assert.Equal(t, day08.IsVisible(g, 2, 3), true)
+	assert.Equal(t, day08.IsVisible(g, 3, 3), false)
+	assert.Equal(t, day08.IsVisible(g, 1, 3), false)
+}
+
+func TestSolvePartOne(t *testing.T) {
+	for _, tc := range tcs {
+		g := day08.NewGrid(tc.input)
+		v := 0
+		for i, row := range g.Sparse() {
+			for j := range row {
+				if day08.IsVisible(g, j, i) {
+					v += 1
+				}
+			}
+		}
+		assert.Equal(t, v, tc.expectedPartOne)
+	}
+}
 
 func TestSolvePartTwo(t *testing.T) {}
